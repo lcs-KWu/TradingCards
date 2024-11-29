ba6f893a79af69ef53b730dead3100c50577a040
Kevin Wu <KWu@lcs.on.ca>
2024-11-29 09:01:02 -0500

    set up model

---
+++ TradingCards/Model/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  TradingCards
+//
+//  Created by Yixuan Wu on 2024-11-29.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
