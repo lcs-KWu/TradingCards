ea94eba3318feafa33b0fc56621375d4f23e7f06
Kevin Wu <KWu@lcs.on.ca>
2024-11-30 21:44:51 -0500

    complete trading card

---
+++ TradingCards/Shape.swift
@@ -0,0 +1,18 @@
+//
+//  Shape.swift
+//  TradingCards
+//
+//  Created by Yixuan Wu on 2024-11-30.
+//
+
+import SwiftUI
+
+struct Shape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Shape()
+}
